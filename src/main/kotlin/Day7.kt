543a67a2b8e7efc5a57d4c5d4392b44b3b5a1435
David Case <david.case@detroitlabs.com>
2022-12-07 21:08:55 -0500

    day 7, part 1 solution

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,16 +1,27 @@
+import java.io.File
+
 fun main() {
 
+    val input = File("src/main/inputs/Day7.txt")
+        .readText()
+    val fs = FileSystem(input)
+    println("Day 7, part 1: ${fs.getTotalSizeOfSmallDirectories()}")
 
 }
 
+private const val SMALL_DIRECTORY_SIZE = 100000
+
 class FileSystem(input: String) {
+
     var files = mutableListOf(Directory("/"))
     var workingDirectory: Directory = files[0]
-    var previousWorkingDirectory: Directory = files[0]
+    private var directoryNavigationQueue: MutableList<Directory> = mutableListOf(files[0])
+    private var totalSizeOfSmallDirectories = 0
 
     init {
         input.split("\n")
             .forEach {
+//                println(workingDirectory.name)
                 val commandParts = it.split(" ")
                 when (commandParts[0]) {
                     "dir" -> {
@@ -20,17 +31,22 @@ class FileSystem(input: String) {
                     "$" -> {
                         when (commandParts[1]) {
                             "cd" -> {
-                                val target = commandParts[2]
-                                val workingDirectorySnapshot = workingDirectory
-                                workingDirectory = when (target) {
-                                    "/" -> files[0]
-                                    ".." -> previousWorkingDirectory
+                                when (val target = commandParts[2]) {
+                                    "/" -> {
+                                        directoryNavigationQueue = mutableListOf(files[0])
+                                        workingDirectory = directoryNavigationQueue.last()
+                                    }
+                                    ".." -> {
+                                        directoryNavigationQueue = directoryNavigationQueue.dropLast(1)
+                                            .toMutableList()
+                                        workingDirectory = directoryNavigationQueue.last()
+                                    }
                                     else -> {
                                         val destinationDirectory = getDestinationDirectory(target)
-                                        destinationDirectory as Directory
+                                        directoryNavigationQueue.add(destinationDirectory as Directory)
+                                        workingDirectory = directoryNavigationQueue.last()
                                     }
                                 }
-                                previousWorkingDirectory = workingDirectorySnapshot
                             }
                         }
                     }
@@ -42,19 +58,45 @@ class FileSystem(input: String) {
                     }
                 }
             }
-//        println(files)
     }
 
     private fun getDestinationDirectory(directoryName: String): FileSystemItem {
         val destinationDirectory = workingDirectory.files.filter { dir ->
             dir.name == directoryName
         }
-        print("destination directory ${destinationDirectory}")
+//        print("destination directory ${destinationDirectory}")
         if (destinationDirectory.isEmpty()) {
             workingDirectory.addDirectory(directoryName)
             return getDestinationDirectory(directoryName)
         }
         return destinationDirectory[0]
+    }
+
+    private fun updateTotalSizeOfSmallDirectories(files: List<Directory>) {
+//        return files.fold(0) { acc, file ->
+////            println(file)
+//            var total = acc
+//            if (file is Directory && file.files.filterIsInstance<Directory>().isNotEmpty()) {
+//                total += getSmallDirectories(file.files)
+//            }
+//            total += if (file.size < SMALL_DIRECTORY_SIZE) file.size else 0
+//            total
+//        }
+        files.forEach {
+            if (it.size < SMALL_DIRECTORY_SIZE) {
+                totalSizeOfSmallDirectories += it.size
+            }
+            if (it.files.filterIsInstance<Directory>().isNotEmpty()) {
+                updateTotalSizeOfSmallDirectories(it.files.filterIsInstance<Directory>())
+            }
+        }
+    }
+
+    fun getTotalSizeOfSmallDirectories(): Int {
+        val directories = files.filterIsInstance<Directory>()
+        updateTotalSizeOfSmallDirectories(directories)
+        println(totalSizeOfSmallDirectories)
+        return totalSizeOfSmallDirectories
     }
 }
 
@@ -64,6 +106,8 @@ open class FileSystemItem(open val name: String, open val size: Int = 0) {
 
 class Directory(override val name: String) : FileSystemItem(name) {
     val files = mutableListOf<FileSystemItem>()
+    override val size: Int
+        get() = files.sumOf { it.size }
 
     fun addDirectory(name: String) {
         files.add(Directory(name))
@@ -71,6 +115,11 @@ class Directory(override val name: String) : FileSystemItem(name) {
 
     fun addFile(name: String, size: Int) {
         files.add(CustomFile(name, size))
+    }
+
+    fun getSmallDirectoriesSize(): Int {
+        return files.filter { it is Directory && it.size < SMALL_DIRECTORY_SIZE }
+            .size
     }
 }
 
--- src/test/kotlin/Day7.kt
+++ src/test/kotlin/Day7.kt
@@ -38,6 +38,8 @@ class Day7_Tests {
     @Test
     fun FileSystem_shouldNavigateToPreviousDirectory() {
         val input = "$ cd a\n" +
+                "$ cd b\n" +
+                "$ cd ..\n" +
                 "$ cd .."
         val fs = FileSystem(input)
         assertEquals(fs.files[0], fs.workingDirectory)
@@ -52,11 +54,56 @@ class Day7_Tests {
                 "8504156 c.dat\n" +
                 "dir d"
         val fs = FileSystem(input)
-        println("test")
-        println(fs.files[0].files)
-        println(fs.workingDirectory)
         assertEquals("b.txt", fs.files[0].files[1].name)
         assertEquals(14848514, fs.files[0].files[1].size)
+    }
+
+    @Test
+    fun FileSystem_shouldCalculateFilesizeOfDirectories() {
+        val input = "1000 a.txt\n" +
+                "2000 b.txt"
+        val fs = FileSystem(input)
+        assertEquals(3000, fs.files[0].size)
+    }
+
+    @Test
+    fun FileSystem_shouldCalculateFilesizeIncludingNestedDirectories() {
+        val input = "1000 a.txt\n" +
+                "2000 b.txt\n" +
+                "$ cd a\n" +
+                "5000 c.txt"
+        val fs = FileSystem(input)
+        assertEquals(8000, fs.files[0].size)
+    }
+
+    @Test
+    fun FileSystem_shouldCalculateTotalSizeOfSmallDirectories() {
+        val input = "\$ cd /\n" +
+                "\$ ls\n" +
+                "dir a\n" +
+                "14848514 b.txt\n" +
+                "8504156 c.dat\n" +
+                "dir d\n" +
+                "\$ cd a\n" +
+                "\$ ls\n" +
+                "dir e\n" +
+                "29116 f\n" +
+                "2557 g\n" +
+                "62596 h.lst\n" +
+                "\$ cd e\n" +
+                "\$ ls\n" +
+                "584 i\n" +
+                "\$ cd ..\n" +
+                "\$ cd ..\n" +
+                "\$ cd d\n" +
+                "\$ ls\n" +
+                "4060174 j\n" +
+                "8033020 d.log\n" +
+                "5626152 d.ext\n" +
+                "7214296 k"
+        val fs = FileSystem(input)
+        val result = fs.getTotalSizeOfSmallDirectories()
+        assertEquals(95437, result)
     }
 
 }
